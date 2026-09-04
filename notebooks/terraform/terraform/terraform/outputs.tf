d98bec6dd71aec89cd9afea178b71e92ea212141
skrishh05 <saikrishnapala3@gmail.com>
2026-09-04 09:02:15 -0400

    Add outputs for environment and project name in Terraform

---
+++ notebooks/terraform/terraform/terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "environment" {
+  description = "Configured deployment environment"
+  value       = var.environment
+}
+
+output "project_name" {
+  description = "Project name"
+  value       = "Databricks Lakehouse Platform Lab"
+}
