6aa104f142aa15bf09987247b5b2d53ce34e9463
skrishh05 <saikrishnapala3@gmail.com>
2026-09-04 09:20:48 -0400

    Add Terraform provider configuration for Databricks

---
+++ architecture/notebooks/terraform/terraform/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.50"
+    }
+  }
+}
+
+provider "databricks" {
+  host = var.databricks_host
+}
